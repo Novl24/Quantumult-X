ba68ec2749230761f207f5979fadf387a9643819
Novl24 <74337952+Novl24@users.noreply.github.com>
2022-04-27 00:31:37 +0800

    Update pushws.js

--- conf/pushws.js
+++ conf/pushws.js
@@ -46,7 +46,7 @@ async function GetBody() {
 
 async function GetWskey() {
 
-    if ($request && $request.url.indexOf("functionId=getNewRedMarkInfo") >= 0) {
+    if ($request && $request.url.indexOf("functionId=getNewsRedMarkInfo") >= 0) {
 
         if (typeof $request.headers !== 'undefined'){
              modifiedWskey = $request.headers;
